604ff40626813ef47712377c5df488b092efdf1d
Aaron Hipple <aaron@footstepsmarketing.com>
2015-06-23 16:57:26 -0600

    S3 deploy script tweak

--- s3fs-fuse/recipes/configure.rb
+++ s3fs-fuse/recipes/configure.rb
@@ -1,14 +1,16 @@
 # AWS OpsWorks Recipe to configure s3fs-fuse
 
-template "/etc/passwd-s3fs" do
-  source "passwd-s3fs.erb"
-  mode "644"
-  owner "root"
-  group "root"
-  variables({
-    :access_key_id => node[:s3fs][:aws_access_key][:id],
-    :access_key_secret => node[:s3fs][:aws_access_key][:secret],
-  })
+if node[:s3fs][:aws_access_key][:id] != false && node[:s3fs][:aws_access_key][:secret] != false
+  template "/etc/passwd-s3fs" do
+    source "passwd-s3fs.erb"
+    mode "644"
+    owner "root"
+    group "root"
+    variables({
+      :access_key_id => node[:s3fs][:aws_access_key][:id],
+      :access_key_secret => node[:s3fs][:aws_access_key][:secret],
+    })
+  end
 end
 
 template "/etc/ahbe.conf" do
--- s3fs-fuse/recipes/deploy.rb
+++ s3fs-fuse/recipes/deploy.rb
@@ -7,7 +7,8 @@ node[:s3fs][:mounts].each do |mount|
     only_if "which s3fs"
     user "root"
     code <<-EOH
-      s3fs #{bucket} #{directory} -o use_cache=/tmp -o default_acl=public_read -o allow_other -o gid=498 -o uid=498 -o ahbe_conf=/etc/ahbe.conf
+      mkdir --parents "#{directory}"
+      s3fs "#{bucket}" "#{directory}" -o use_cache=/tmp -o default_acl=public_read -o allow_other -o gid=498 -o uid=498 -o ahbe_conf=/etc/ahbe.conf
     EOH
   end
 end
