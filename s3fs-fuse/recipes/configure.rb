712c37b7380edee4dd26759cb9c9ebd22968ffe0
Aaron Hipple <aaron@footstepsmarketing.com>
2015-06-24 11:44:35 -0600

    Configure tinkering

--- s3fs-fuse/recipes/configure.rb
+++ s3fs-fuse/recipes/configure.rb
@@ -1,17 +1,17 @@
 # AWS OpsWorks Recipe to configure s3fs-fuse
 
-if node[:s3fs][:aws_access_key][:id] != false && node[:s3fs][:aws_access_key][:secret] != false
-  template "/etc/passwd-s3fs" do
-    source "passwd-s3fs.erb"
-    mode "600"
-    owner "root"
-    group "root"
-    variables({
-      :access_key_id => node[:s3fs][:aws_access_key][:id],
-      :access_key_secret => node[:s3fs][:aws_access_key][:secret],
-    })
-  end
+
+template "/etc/passwd-s3fs" do
+  source "passwd-s3fs.erb"
+  mode "600"
+  owner "root"
+  group "root"
+  variables({
+    :access_key_id => node[:s3fs][:aws_access_key][:id],
+    :access_key_secret => node[:s3fs][:aws_access_key][:secret],
+  })
 end
+
 
 template "/etc/ahbe.conf" do
   source "ahbe.conf.erb"
