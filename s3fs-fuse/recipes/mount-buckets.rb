4f98e2a285e86a172ca6abd0644ee5174be6bae3
Aaron Hipple <aaron@footstepsmarketing.com>
2015-06-24 14:31:18 -0600

    Maybe get user and group properly now

--- s3fs-fuse/recipes/mount-buckets.rb
+++ s3fs-fuse/recipes/mount-buckets.rb
@@ -1,12 +1,14 @@
 # AWS OpsWorks Recipe to configure s3fs-fuse
 
 node[:s3fs][:mounts].each do |bucket, directory|
+  user = node[:opsworks][:deploy_user][:user]
+  group = node[:opsworks][:deploy_user][:group]
   bash "mount_s3fs" do
     only_if "which s3fs"
     user "root"
     code <<-EOH
       mkdir --parents "#{directory}"
-      s3fs "#{bucket}" "#{directory}" -o use_cache=/tmp -o default_acl=public_read -o allow_other -o gid=$(id -g #{node[:opsworks][:deploy_user][:group]}) -o uid=$(id -g #{node[:opsworks][:deploy_user][:user]}) -o ahbe_conf=/etc/ahbe.conf
+      s3fs "#{bucket}" "#{directory}" -o use_cache=/tmp -o default_acl=public_read -o allow_other -o gid=$(id -g #{group}}) -o uid=$(id -g #{user}}) -o ahbe_conf=/etc/ahbe.conf
     EOH
   end
 end
