f3ac1bc66fbff8d5278dfdb0497b06ad03282187
Aaron Hipple <aaron@footstepsmarketing.com>
2015-07-06 13:32:38 -0600

    Updating wordpress cookbook

--- wordpress/attributes/default.rb
+++ wordpress/attributes/default.rb
@@ -6,3 +6,21 @@ default['wordpress']['wp_config']['enable_W3TC'] = false
 
 # Force logins via https (http://codex.wordpress.org/Administration_Over_SSL#To_Force_SSL_Logins_and_SSL_Admin_Access)
 default['wordpress']['wp_config']['force_secure_logins'] = false
+
+default['wordpress']['wp_config']['sunrise'] = false
+
+default['wordpress']['wp_config']['debug'] = false
+
+default['wordpress']['wp_config']['site_url'] = false
+
+default['wordpress']['wp_config']['fsm_google_public_api_key'] = false
+
+default['wordpress']['wp_config']['multisite']['enabled'] = false
+default['wordpress']['wp_config']['multisite']['subdomain_install'] = false
+default['wordpress']['wp_config']['multisite']['domain_current_site'] = false
+default['wordpress']['wp_config']['multisite']['default_site'] = 1
+
+default['wordpress']['wordfence'] = false
+
+default['wordpress']['max_upload_size'] = '32M'
+default['wordpress']['max_execution_time'] = '180'
