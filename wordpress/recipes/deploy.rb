70495659039b2ea520e4b2d9f78ed6355b5e5334
Aaron Hipple <aaron@footstepsmarketing.com>
2015-11-19 17:16:54 -0700

    Try recursive on for size...

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -9,6 +9,7 @@ node[:deploy].each do |app_name, deploy|
     exclude_plugins.each do |plugin|
         Chef::Log.debug("Deleting #{deploy[:deploy_to]}/current/wp-content/plugins/#{plugin}")
         directory "#{deploy[:deploy_to]}/current/wp-content/plugins/#{plugin}" do
+            recursive true
             action :delete
         end
     end
@@ -16,6 +17,7 @@ node[:deploy].each do |app_name, deploy|
     exclude_themes.each do |theme|
         Chef::Log.debug("#{deploy[:deploy_to]}/current/wp-content/themes/#{theme}")
         directory "#{deploy[:deploy_to]}/current/wp-content/themes/#{theme}" do
+            recursive true
             action :delete
         end
     end
