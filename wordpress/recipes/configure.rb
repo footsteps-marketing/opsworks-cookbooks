2e04da383b0f4ba8aaa03cc8458b23293fb365dc
Aaron Hipple <aaron@footstepsmarketing.com>
2015-12-18 15:58:46 -0700

    Recipes for letsencrypt stuff!

--- wordpress/recipes/configure.rb
+++ wordpress/recipes/configure.rb
@@ -95,7 +95,57 @@ node[:deploy].each do |app_name, deploy|
         EOH
     end
 
+    template "#{deploy[:deploy_to]}/current/get-mapped-domains.php" do
+        source "get-mapped-domains.php.erb"
+        mode 0700
+        owner "root"
+    end
 
+    ruby_block "something" do
+        block do
+            #tricky way to load this Chef::Mixin::ShellOut utilities
+            Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
+            command = "php #{deploy[:deploy_to]}/current/get-mapped-domains.php"
+            command_out = shell_out(command)
+            node.set['wp_mapped_domains'] = command_out.stdout
+        end
+        action :create
+    end
+
+    node[:wp_mapped_domains].unshift("#{node[:wordpress][:wp_config][:multisite][:domain_current_site]}")
+
+    script "letsencrypt_doer" do
+        interpreter "bash"
+        user "root"
+        cwd "#{deploy[:deploy_to]}/letsencrypt"
+        code <<-EOH
+            DOMAINS="-d #{node[:wordpress][:wp_config][:multisite][:domain_current_site]}"
+            for domain in $(php #{deploy[:deploy_to]}/current/get-mapped-domains.php); do
+                DOMAINS="${DOMAINS} -d ${domain}"
+            done
+            ./letsencrypt-auto certonly --keep --webroot -w "#{deploy[:deploy_to]}/current" $DOMAINS
+        EOH
+    end
+
+    node[:wp_mapped_domains].each do |mapped_domain|
+        
+        template "#{node[:apache][:dir]}/sites-enabled/#{mapped_domain}.conf" do
+            source 'mapped_domain.conf.erb'
+            owner 'root'
+            group 'root'
+            mode 0644
+            variables :domain => mapped_domain
+        end
+        
+    end
+
+    script "restart_after_letsencrypt" do
+        interpreter "bash"
+        user "root"
+        code <<-EOH
+            service apache2 restart
+        EOH
+    end
 
 	# Import Wordpress database backup from file if it exists
 	mysql_command = "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"} #{deploy[:database][:database]}"
