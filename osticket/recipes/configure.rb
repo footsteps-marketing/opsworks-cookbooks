3158bc45fb12fa21452d88d3d43d5764ce4b3cee
Aaron Hipple <aaron@footstepsmarketing.com>
2016-01-19 15:30:59 -0700

    Update for osticket recipe -- only deploy config on osticket installs

--- osticket/recipes/configure.rb
+++ osticket/recipes/configure.rb
@@ -1,24 +1,26 @@
 node[:deploy].each do |app_name, deploy|
 
-    template "#{deploy[:deploy_to]}/include/ost-config.php" do
-        source "ost-config.php.erb"
-        mode 0660
-        group deploy[:group]
+    if (app_name == 'osTicket')
+        template "#{deploy[:deploy_to]}/include/ost-config.php" do
+            source "ost-config.php.erb"
+            mode 0660
+            group deploy[:group]
 
-        if platform?("ubuntu")
-            owner "deploy"
-        elsif platform?("amazon")
-            owner "apache"
+            if platform?("ubuntu")
+                owner "deploy"
+            elsif platform?("amazon")
+                owner "apache"
+            end
+
+            variables(
+                :database     => (deploy[:database][:database] rescue nil),
+                :user         => (deploy[:database][:username] rescue nil),
+                :password     => (deploy[:database][:password] rescue nil),
+                :host         => (deploy[:database][:host] rescue nil),
+                :secret_salt  => (deploy[:osticket][:secret_salt] rescue nil),
+                :admin_email  => (deploy[:osticket][:admin_email] rescue nil)
+            )
         end
-
-        variables(
-            :database     => (deploy[:database][:database] rescue nil),
-            :user         => (deploy[:database][:username] rescue nil),
-            :password     => (deploy[:database][:password] rescue nil),
-            :host         => (deploy[:database][:host] rescue nil),
-            :secret_salt  => (deploy[:osticket][:secret_salt] rescue nil),
-            :admin_email  => (deploy[:osticket][:admin_email] rescue nil)
-        )
     end
     
 end
