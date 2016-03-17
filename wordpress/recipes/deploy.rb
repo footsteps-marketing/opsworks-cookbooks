bfc319142ff7bdafdaef304e94c7669d707e260e
Aaron Hipple <aaron@footstepsmarketing.com>
2016-03-17 17:01:01 -0600

    Maybe absolute paths in there

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -57,7 +57,7 @@ node[:deploy].each do |app_name, deploy|
             for domain in $(php #{deploy[:deploy_to]}/current/get-mapped-domains.php); do
                 DOMAINS="${DOMAINS} -d ${domain}"
             done
-            ./letsencrypt-auto certonly --webroot --text --keep-until-expiring --agree-tos --email "#{node[:wordpress][:letsencrypt][:admin_email]}" --webroot-path "#{deploy[:deploy_to]}/current" $DOMAINS
+            /opt/letsencrypt/letsencrypt-auto certonly --webroot --text --keep-until-expiring --agree-tos --email "#{node[:wordpress][:letsencrypt][:admin_email]}" --webroot-path "#{deploy[:deploy_to]}/current" $DOMAINS
         EOH
     end
 
