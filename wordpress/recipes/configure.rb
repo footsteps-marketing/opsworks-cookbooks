4ad375bc6a2356dca7eecfd74a3d90d18cd178ae
Aaron Hipple <aaron@footstepsmarketing.com>
2016-03-17 15:24:29 -0600

    Playing with letsencrypt again

--- wordpress/recipes/configure.rb
+++ wordpress/recipes/configure.rb
@@ -129,69 +129,11 @@ node[:deploy].each do |app_name, deploy|
     end
 
 
-=begin
-    git "#{deploy[:deploy_to]}/letsencrypt" do
+    git "/opt/letsencrypt" do
         user "root"
         repository "https://github.com/letsencrypt/letsencrypt.git"
         action :sync
     end
-
-
-    script "letsencrypt_init" do
-        interpreter "bash"
-        user "root"
-        cwd "#{deploy[:deploy_to]}/letsencrypt"
-        code "screen ./letsencrypt-auto --help 2&1 >> ../letsencrypt-output.log"
-    end
-    
-
-    ruby_block "check_curl_command_output" do
-        block do
-            #tricky way to load this Chef::Mixin::ShellOut utilities
-            Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
-            command = "php #{deploy[:deploy_to]}/current/get-mapped-domains.php"
-            command_out = shell_out(command)
-            node[:wp_mapped_domains] = command.stdout.split("\n")
-        end
-        action :create
-    end
-
-    node[:wp_mapped_domains].unshift("#{node[:wordpress][:wp_config][:multisite][:domain_current_site]}")
-
-    script "letsencrypt_doer" do
-        interpreter "bash"
-        user "root"
-        cwd "#{deploy[:deploy_to]}/letsencrypt"
-        code <<-EOH
-            DOMAINS="-d #{node[:wordpress][:wp_config][:multisite][:domain_current_site]}"
-            for domain in $(php #{deploy[:deploy_to]}/current/get-mapped-domains.php); do
-                DOMAINS="${DOMAINS} -d ${domain}"
-            done
-            ./letsencrypt-auto certonly --keep --webroot -w "#{deploy[:deploy_to]}/current" $DOMAINS
-        EOH
-    end
-
-    node[:wp_mapped_domains].each do |mapped_domain|
-        
-        template "#{node[:apache][:dir]}/sites-enabled/#{mapped_domain}.conf" do
-            source 'mapped_domain.conf.erb'
-            owner 'root'
-            group 'root'
-            mode 0644
-            variables :domain => mapped_domain
-        end
-        
-    end
-
-    script "restart_after_letsencrypt" do
-        interpreter "bash"
-        user "root"
-        code <<-EOH
-            service apache2 restart
-        EOH
-    end
-=end
-
 
 	# Import Wordpress database backup from file if it exists
 	mysql_command = "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"} #{deploy[:database][:database]}"
