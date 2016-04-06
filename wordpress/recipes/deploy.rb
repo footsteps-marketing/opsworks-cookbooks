aa811f310779d0d47b5902c0fc3bc0ddb574290f
Aaron Hipple <aaron@footstepsmarketing.com>
2016-04-06 12:53:08 -0600

    Better variable-ing and rewriting of stuff

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -62,24 +62,39 @@ node[:deploy].each do |app_name, deploy|
     end
 
     domains_to_map.each do |mapped_domain|
+
+        application_name = params[:name]
+        directory "#{node[:apache][:dir]}/sites-available/#{application_name}.conf.d"
+        params[:rewrite_config] = "#{node[:apache][:dir]}/sites-available/#{application_name}.conf.d/rewrite"
+        params[:local_config] = "#{node[:apache][:dir]}/sites-available/#{application_name}.conf.d/local"
         
-        template "#{node[:apache][:dir]}/sites-enabled/#{mapped_domain}.conf" do
+        template "#{node[:apache][:dir]}/sites-available/#{mapped_domain}.conf" do
             source 'mapped_domain.conf.erb'
             owner 'root'
             group 'root'
-            mode 0644            
+            mode 0644
+            if params[:cookbook]
+                cookbook params[:cookbook]
+            end
+            environment_variables = if node[:deploy][application_name].nil?
+                                        {}
+                                    else
+                                        node[:deploy][application_name][:environment_variables]
+                                    end
+            variables(
+                :application_name => application_name,
+                :mapped_domain => mapped_domain,
+                :params => params,
+                :environment => OpsWorks::Escape.escape_double_quotes(environment_variables)
+            )
+            if ::File.exists?("#{node[:apache][:dir]}/sites-enabled/#{application_name}.conf")
+                notifies :reload, "service[apache2]", :delayed
+            end
+        end
+        apache_site "#{mapped_domain}.conf" do
+            enable enable_setting
         end
         
     end
-
-    script "restart_after_letsencrypt" do
-        interpreter "bash"
-        user "root"
-        code <<-EOH
-            service apache2 reload
-        EOH
-    end
-
-
 
 end
