272d4b3e8917ee5492fe18010639fa70de71838a
Aaron Hipple <aaron@footstepsmarketing.com>
2016-04-06 14:43:46 -0600

    Maybe it likes dup better

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -63,7 +63,7 @@ node[:deploy].each do |app_name, deploy|
 
     domains_to_map.each do |mapped_domain|
 
-        params = deploy.clone
+        params = deploy.dup
 
         application_name = params[:name]
         directory "#{node[:apache][:dir]}/sites-available/#{application_name}.conf.d"
